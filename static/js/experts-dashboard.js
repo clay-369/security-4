e530767529672421aaed63027ec9ad03e92e1817
yorick-1101625 <1101625@hr.nl>
2025-02-02 20:15:37 +0100

    Experts: Made page toggler functionality

---
+++ static/js/experts-dashboard.js
@@ -0,0 +1,21 @@
+// Page toggler
+
+document.querySelectorAll('.js-page-toggler')
+    .forEach(togglerElement => {
+        togglerElement.addEventListener('click' , () => {
+            toggleSections();
+        });
+    });
+
+function toggleSections() {
+    const researchSection = document.querySelector('.js-research-section');
+    const enlistmentsSection = document.querySelector('.js-enlistments-section');
+
+    if (researchSection.classList.contains('hide')) {
+        researchSection.classList.remove('hide');
+        enlistmentsSection.classList.add('hide');
+    } else {
+        researchSection.classList.add('hide');
+        enlistmentsSection.classList.remove('hide');
+    }
+}
