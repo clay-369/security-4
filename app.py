f563879f854cb12685ad8efd3d4c3c4d4173646a
yorick-1101625 <1101625@hr.nl>
2025-02-05 18:53:02 +0100

    Fixed sql queries in research.py and made a test fetch request

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask, render_template, request
 # DB Models
 from lib.model.research import Research
@@ -7,6 +8,10 @@ app = Flask(__name__)
 @app.route('/')
 def login():
     return render_template('log-in.html')
+
+@app.route('/test')
+def test():
+    return render_template('api-test.html')
 
 @app.route('/api/onderzoeken', methods=['POST'])
 def create_research_item():
@@ -18,7 +23,8 @@ def create_research_item():
     if not new_research_id:
         return # Error code
 
-    new_research_item = research_model.get_research_by_id(new_research_id)
+    new_research_item = dict(research_model.get_research_by_id(new_research_id))
+
     return new_research_item, 201
 
 if __name__ == "__main__":
--- lib/model/research.py
+++ lib/model/research.py
@@ -3,18 +3,14 @@ from lib.model.database import Database
 
 class Research:
     def __init__(self):
-        database = Database("../../databases/database.db")
+        database = Database("./databases/database.db")
         self.conn, self.cursor = database.connect_db()
 
-    def get_max_id(self):
-        max_id = self.cursor.execute("SELECT MAX(id) FROM onderzoeken").fetchone()
-        return max_id
-
     def get_research_by_id(self, research_id:int):
-        result = self.cursor.execute("SELECT * FROM onderzoeken WHERE id = ?", research_id).fetchone()
+        result = self.cursor.execute("SELECT * FROM onderzoeken WHERE onderzoek_id = ?", (research_id,)).fetchone()
         return result
 
-    def create_research(self, research_item:object):
+    def create_research(self, research_item:dict):
 
 
         # Accept dictionary like
@@ -52,26 +48,25 @@ class Research:
         if research_item['onderzoek_type'] not in ['ONLINE', 'OP LOCATIE', 'TELEFONISCH']:
             return False
 
-        new_research_id = self.get_max_id()
+        self.cursor.execute("""
+        INSERT INTO onderzoeken 
+        (
+            organisatie_id, titel, beschikbaar, datum_vanaf, datum_tot, onderzoek_type, locatie, 
+            met_beloning, beloning, leeftijd_vanaf, leeftijd_tot, status
+        ) 
+        VALUES (?,?,?,?,?,?,?,?,?,?,?,?)""",
+        (
+            research_item['organisatie_id'], research_item['titel'], research_item['beschikbaar'],
+            research_item['datum_vanaf'], research_item['datum_tot'], research_item['onderzoek_type'],
+            research_item['locatie'], research_item['met_beloning'], research_item['beloning'],
+            research_item['leeftijd_vanaf'], research_item['leeftijd_tot'], research_item['status']
+        ))
 
-        self.cursor.execute(
-            "INSERT into onderzoeken ("
-            "onderzoek_id, organisatie_id, titel, beschikbaar, "
-            "datum_vanaf, datum_tot, onderzoek_type, locatie, met_beloning, "
-            "beloning, leeftijd_vanaf, leeftijd_tot, status"
-            ") "
-            "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",
-            (
-                new_research_id,
-                research_item['organisatie_id'], research_item['titel'], research_item['beschikbaar'],
-                research_item['datum_vanaf'], research_item['datum_tot'], research_item['onderzoek_type'],
-                research_item['locatie'], research_item['met_beloning'], research_item['beloning'],
-                research_item['leeftijd_vanaf'], research_item['leeftijd_tot'], research_item['status']
-            ),
-        )
         self.conn.commit()
 
+        new_research_id = self.cursor.lastrowid
+
         # TODO: create beperking_onderzoek instances
-        # for item in research_item['beperingen']
+        # for item in research_item['beperkingen']
 
         return new_research_id
