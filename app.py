23fa6785b6676a970f84b31d945282c75e26562f
daan-1112062 <1112062@hr.nl>
2025-02-05 12:24:13 +0100

    Organisatie Registratie html page

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def login():
     return render_template('log-in.html')
 
+@app.route('/organisatie_registratie')
+def organisatie_registratie():
+    return render_template('organisatie_registratie.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
