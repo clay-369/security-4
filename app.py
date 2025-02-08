22b2916447aec697edb82e9aa473df849327c1b0
daan-1112062 <1112062@hr.nl>
2025-02-09 00:07:08 +0100

    Fixed pathing beheerder beheer + css
    Css is working now and also the route for beheerder beheer

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def login():
     return render_template('log-in.html')
 
+@app.route('/beheerder_beheer')
+def beheerder():
+    return render_template('beheerder-beheer.html')
+
 @app.route('/dashboard_beheer')
 def dashboard_beheer():
     return render_template('dashboard-beheer.html')
