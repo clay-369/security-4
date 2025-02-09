eee54f1ea0b4ea8b049e62eedf37fc5b7533aee7
yorick-1101625 <1101625@hr.nl>
2025-02-09 19:16:17 +0100

    Refactored experts-dashboard.js and removed module files

--- static/js/experts-dashboard.js
+++ static/js/experts-dashboard.js
@@ -1,10 +1,11 @@
-import {renderResearchPage} from "./experts-dashboard/research-page.js";
-import {renderEnlistmentPage} from "./experts-dashboard/enlistment-page.js";
 
+// TODO: Switch to sessions
+const expertId = 1;
 
 // Get research items from database and call the render function
 let allResearchItems = [];
 
+// Load all research items
 fetch('/api/onderzoeken', {
     method: 'GET',
     headers: {
@@ -33,11 +34,223 @@ function toggleSections() {
         researchSection.classList.remove('hide');
         enlistmentsSection.classList.add('hide');
 
-        renderResearchPage(allResearchItems);
+        renderResearchPage();
     } else {
         researchSection.classList.add('hide');
         enlistmentsSection.classList.remove('hide');
 
-        renderEnlistmentPage(allResearchItems);
+        renderEnlistmentPage();
     }
+}
+
+
+// TODO: Only show researches that have not been interacted with by this user
+
+// Research page
+function renderResearchPage() {
+
+    let html = '';
+    allResearchItems.forEach(researchItem => {
+        html += `
+            <div class="research-container js-research-container" data-research-id="${researchItem.onderzoek_id}">
+                <h2 class="research-title">${researchItem.titel}</h2>
+                <div class="research-details">
+                    <p><time>${researchItem.datum_vanaf}</time> tot <time>${researchItem.datum_tot}</time></p>
+                    <p>${researchItem.onderzoek_type.toLowerCase()}</p>
+                </div>
+                <div class="research-details">
+                    <p>${researchItem.met_beloning ? 'Met beloning' : 'Zonder beloning'}</p>
+                    <p>${researchItem.organisatie_naam}</p>
+                </div>
+            </div>
+        `;
+    });
+
+    document.querySelector('.js-research-grid-container')
+        .innerHTML = html;
+
+    // Open research modal event listener
+    document.querySelectorAll('.js-research-container')
+    .forEach(containerElement => {
+        const researchId = containerElement.dataset.researchId;
+
+        containerElement.addEventListener('click', () => {
+            renderResearchModal(researchId);
+        });
+    });
+}
+
+// Research Modal
+function renderResearchModal(researchId, enlistmentId=null) {
+    let matchingResearchItem;
+
+    allResearchItems.forEach(researchItem => {
+        if (Number(researchItem['onderzoek_id']) === Number(researchId)) {
+            matchingResearchItem = researchItem;
+        }
+    });
+
+    document.querySelector('.js-research-modal-background')
+        .innerHTML = `
+            <div class="research-modal">
+                <img class="close-modal js-close-modal" alt="Sluit Popup" src="../static/icons/xmark-solid.svg">
+                <h1 class="research-modal-title">${matchingResearchItem.titel}</h1>
+                <p class="research-modal-description">${matchingResearchItem.beschrijving}</p>
+                <h2 class="research-modal-organisation">Details</h2>
+                <div class="research-modal-details">
+                    <p>Datum</p>
+                    <p><time>${matchingResearchItem.datum_vanaf}</time> tot <time>${matchingResearchItem.datum_tot}</time></p>
+            
+                    <p>Waar</p>
+                    <p>${matchingResearchItem.onderzoek_type.toLowerCase()}</p>
+                    
+                    <p>Beloning</p>
+                    <p>${matchingResearchItem.met_beloning ? matchingResearchItem.beloning : 'Zonder beloning'}</p>
+                    
+                    <p>Leeftijd</p>
+                    <p>${matchingResearchItem.leeftijd_vanaf} tot ${matchingResearchItem.leeftijd_tot} jaar</p>
+                    
+                    <p>Organisatie</p>
+                    <p>${matchingResearchItem.organisatie_naam}</p>
+                </div>
+                
+                ${
+                    enlistmentId
+                    ? '<button class="research-modal-button red js-enlist-button">Uitschrijven</button>'
+                    : '<button class="research-modal-button js-enlist-button">Inschrijven</button>'
+                }                
+                
+                
+            </div>
+        `;
+
+    document.querySelector('.js-research-modal-background')
+        .classList.remove('hide');
+
+    // Close research modal event listener
+    document.querySelector('.js-close-modal')
+    .addEventListener('click', closeResearchModal);
+
+    // Enlist event listener
+    document.querySelector('.js-enlist-button')
+        .addEventListener('click', () => {
+            if (enlistmentId) {
+                delist(enlistmentId);
+            } else {
+                enlist(researchId);
+            }
+
+        })
+}
+
+function closeResearchModal() {
+    document.querySelector('.js-research-modal-background')
+           .classList.add('hide');
+}
+
+function enlist(researchId) {
+    fetch('/api/onderzoeken/inschrijvingen', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({research_id: researchId, expert_id: expertId})
+        })
+            .then(closeResearchModal);
+}
+
+function delist(enlistmentId) {
+    fetch('/api/onderzoeken/inschrijvingen', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({enlistment_id: enlistmentId})
+        })
+            .then(closeResearchModal)
+            .then(() => {
+                renderEnlistmentPage();
+            });
+}
+
+
+// Enlistment Page
+function renderEnlistmentPage() {
+    // Getting all enlistments
+    fetch('/api/onderzoeken/inschrijvingen', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Rendering each partition
+            renderPartitions(data);
+        });
+}
+
+function renderPartitions(enlistments) {
+    let pendingHTML = '';
+    let acceptedHTML = '';
+    let rejectedHTML = '';
+
+    enlistments.forEach((enlistment) => {
+        // Pending
+        if (enlistment['deskundige_id'] === expertId && enlistment['status'] === 'NIEUW') {
+            pendingHTML += `
+                <div class="enlistment">
+                    <h3 class="enlistment-name">${enlistment['titel']}</h3>
+                    <button class="enlistment-details-button js-enlistment-details-button" 
+                        data-research-id="${enlistment['onderzoek_id']}"
+                        data-enlistment-id="${enlistment['inschrijving_id']}"
+                    >Details</button>
+                </div>
+            `;
+        }
+        // Accpeted
+        else if (enlistment['deskundige_id'] === expertId && enlistment['status'] === 'GOEDGEKEURD') {
+            acceptedHTML += `
+                <div class="enlistment">
+                    <h3 class="enlistment-name">${enlistment['titel']}</h3>
+                    <button class="enlistment-details-button js-enlistment-details-button" 
+                        data-research-id="${enlistment['onderzoek_id']}"
+                        data-enlistment-id="${enlistment['inschrijving_id']}"
+                    >Details</button>
+                </div>
+            `;
+        }
+        // Rejected
+        else if (enlistment['deskundige_id'] === expertId && enlistment['status'] === 'AFGEKEURD') {
+            rejectedHTML += `
+                <div class="enlistment">
+                    <h3 class="enlistment-name">${enlistment['titel']}</h3>
+                    <button class="enlistment-details-button js-enlistment-details-button" 
+                        data-research-id="${enlistment['onderzoek_id']}"
+                        data-enlistment-id="${enlistment['inschrijving_id']}"
+                    >Details</button>
+                </div>
+            `;
+        }
+    })
+
+    document.querySelector('.js-pending-enlistments')
+        .innerHTML = pendingHTML;
+
+    document.querySelector('.js-accepted-enlistments')
+        .innerHTML = acceptedHTML;
+
+    document.querySelector('.js-rejected-enlistments')
+        .innerHTML = rejectedHTML;
+
+    // Eventlisteners
+    document.querySelectorAll('.js-enlistment-details-button')
+        .forEach(button => {
+            const researchId = button.dataset.researchId;
+            const enlistmentId = button.dataset.enlistmentId;
+
+            button.addEventListener('click', () => {
+                renderResearchModal(researchId, enlistmentId);
+            });
+        });
 }
