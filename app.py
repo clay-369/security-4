3d624b0d894aa66e304cc0ccfa96b5c52fb29b05
Timothy-1031438 <124897596+t3xj@users.noreply.github.com>
2025-02-12 11:34:39 +0100

    Added pop up box form to create new admin (CSS not working with me, needs to be fixed)

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def login():
     return render_template('log-in.html')
+@app.route('/admin/beheer')
+def beheer():
+    return render_template('beheerder-beheer.html')
+@app.route('/admin/nieuw')
+def admin_nieuw():
+    return render_template('nieuwe-admin.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/js/nieuwe-admin.js
@@ -0,0 +1,7 @@
+function openModal() {
+    document.getElementById("createModal").style.display = "flex";
+}
+
+function closeModal() {
+    document.getElementById("createModal").style.display = "none";
+}
