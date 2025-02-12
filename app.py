22f65a55c459dada16e507c0561c44b9d8b3cbda
Timothy-1031438 <124897596+t3xj@users.noreply.github.com>
2025-02-12 13:13:13 +0100

    Added functionality to create new admins

--- app.py
+++ app.py
@@ -21,9 +21,21 @@ def login():
             return jsonify({"success": False})
     else:
         return render_template('log-in.html')
-@app.route('/admin/beheer')
+@app.route('/admin/beheer', methods=['GET','POST'])
 def beheer():
-    return render_template('beheerder-beheer.html')
+    if request.method == 'POST':
+        print('dsa sd')
+        data = request.get_json()
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        email = data.get('email')
+        password = data.get('password')
+        users_model = Users()
+        create_admin = users_model.admin_create(first_name, last_name, email, password)
+        if create_admin:
+            return jsonify({"success": True})
+    else:
+        return render_template('beheerder-beheer.html')
 @app.route('/user')
 def user():
     return render_template('experts-dashboard.html')
--- static/js/nieuwe-admin.js
+++ static/js/nieuwe-admin.js
@@ -5,3 +5,31 @@ function openModal() {
 function closeModal() {
     document.getElementById("createModal").style.display = "none";
 }
+
+document.getElementById('createAdmin').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const first_name = document.getElementById("first_name").value;
+    const last_name = document.getElementById("last_name").value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    console.log(first_name)
+
+    fetch('/admin/beheer', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({first_name: first_name, last_name: last_name, email: email, password: password})
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            console.log('Admin gemaakt!')
+            closeModal()
+        } else {
+            console.log('Error!')
+        }
+    })
+});
