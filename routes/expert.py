860214125e455dc334963d78163f15c32d795e97
yorick-1101625 <1101625@hr.nl>
2025-02-20 13:54:35 +0100

    Moved routes to blueprints, Fixed login redirects with new blueprints

---
+++ routes/expert.py
@@ -0,0 +1,60 @@
+from flask import Blueprint, render_template, request
+
+from lib.model.deskundige import Deskundige
+
+expert_bp = Blueprint('expert', __name__)
+
+@expert_bp.route('/deskundige')
+def dashboard():
+    return render_template('experts-dashboard.html')
+
+
+@expert_bp.route('/deskundige/registreer')
+def register():
+    return render_template('registreer_deskundige.html')
+
+
+@expert_bp.route('/deskundige/profiel')
+def details():
+    return render_template('deskundige_details.html')
+
+
+@expert_bp.route('/deskundige/profiel/bewerk')
+def edit():
+    return render_template('account_beheer.html')
+
+
+# API
+@expert_bp.route("/api/deskundige", methods=["GET", "POST", "PUT"])
+def deskundige_api():
+    deskundige = Deskundige()
+    if request.method == 'POST':
+        data = request.get_json()
+        create_admin = deskundige.create_deskundige(data)
+
+        if create_admin:
+            return {"success": True}
+        else:
+            return {"success": False}
+
+    if request.method == 'GET' and request.args.get('id'):
+        print("test")
+        deskundige_id = request.args.get('id')
+        deskundige_info = deskundige.get_single_deskundige(deskundige_id)
+        single_deskundige_dict = dict(deskundige_info)
+
+        if deskundige_info:
+            return {"success": True, "deskundige": single_deskundige_dict}
+        else:
+            return {"success": False}
+
+    if request.method == 'PUT' and request.args.get('id'):
+        deskundige_id = request.args.get('id')
+        data = request.get_json()
+        update_deskundige = deskundige.update_deskundige(data)
+
+        if update_deskundige:
+            return {"success": True}
+        else:
+            return {"success": False}
+
