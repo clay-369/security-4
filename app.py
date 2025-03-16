049ab1a1755dd45f10666737ecaf15d7806dfd73
yorick-1101625 <1101625@hr.nl>
2025-03-16 14:53:45 +0100

    Styled dark-mode for login and registration screens

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=30)
 
 Session(app)
 
